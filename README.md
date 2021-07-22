987e55380ddbe9a785241d9aac4765b96199793b
William Penagos <billalpeza@gmail.com>
2021-07-22 14:06:11 -0500

    started

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # action-generate-invalidation
 Create an invalidation cloudfront
+
+example:
+
+```yml
+name: Testing PR
+
+on:
+  pull_request:
+    types: [opened, synchronize]
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: fortil/action-generate-invalidation@v1
+        with:
+          INVALIDATION_ID: INVALIDATION_ID
+```
---
+++ src/action.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+const cloudfront = new AWS.CloudFront();
+const core = require('@actions/core');
+
+const INVALIDATION_ID = core.getInput('INVALIDATION_ID');
+
+async function run() {
+  const params = {
+    DistributionId: `${INVALIDATION_ID}`, /* required */
+    InvalidationBatch: { /* required */
+      CallerReference: `purchase-reference-${Math.floor(Math.random() * 1000)}`, /* required */
+      Paths: { /* required */
+        Quantity: 1, /* required */
+        Items: [
+          '/',
+          /* more items */
+        ]
+      }
+    }
+  };
+  return new Promise((res, rej) => cloudfront.createInvalidation(params, function(err, data) {
+    if (err) rej(err || err.stack); // an error occurred
+    else     res(data);           // successful response
+  }));
+}
+
+run();
